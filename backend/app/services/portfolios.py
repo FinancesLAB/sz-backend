16650d8b9cda474e3b5749be06cc8dcbb42cd226
wisetree911 <gariksharik224@gmail.com>
2026-01-26 01:54:39 +0500

    refactor: привел имя аргумента полезеной нагрузки к общему виду в репозитории, свервисе и контракте портфолио - payload

--- backend/app/services/portfolios.py
+++ backend/app/services/portfolios.py
@@ -16,8 +16,8 @@ class PortfolioService:
             raise HTTPException(404, 'SZ portfolio not found')
         return portfolio
 
-    async def create_portfolio(self, obj_in: PortfolioCreatePublic):
-        return await self.repo.create(obj_in=obj_in)
+    async def create_portfolio(self, payload: PortfolioCreatePublic):
+        return await self.repo.create(payload=payload)
 
     async def delete_portfolio_by_portfolio_id(self, portfolio_id: int):
         portfolio = await self.repo.get_by_id(portfolio_id=portfolio_id)
@@ -30,7 +30,7 @@ class PortfolioService:
         portfolio = await self.repo.get_by_id(portfolio_id=portfolio_id)
         if portfolio is None:
             raise HTTPException(404, 'SZ portfolio not found')
-        await self.repo.update(portfolio=portfolio, obj_in=payload)
+        await self.repo.update(portfolio=portfolio, payload=payload)
         return portfolio
 
     async def get_user_portfolios(self, user_id: int):
--- backend/shared/repositories/portfolio.py
+++ backend/shared/repositories/portfolio.py
@@ -12,8 +12,8 @@ class PortfolioRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
 
-    async def create(self, obj_in: PortfolioCreateAdm):
-        obj = Portfolio(**obj_in.dict())
+    async def create(self, payload: PortfolioCreateAdm):
+        obj = Portfolio(**payload.dict())
         self.session.add(obj)
         await self.session.commit()
         await self.session.refresh(obj)
