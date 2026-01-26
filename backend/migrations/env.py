ac765b6cf97cd5f6086c072e722ad23ce57215c0
wisetree911 <gariksharik224@gmail.com>
2026-01-27 00:19:10 +0500

    fix(alembic): поддерживание миграций во время тестов в лупе asyncio

--- backend/migrations/env.py
+++ backend/migrations/env.py
@@ -1,13 +1,14 @@
+import asyncio
 from logging.config import fileConfig
+
 from alembic import context
 from sqlalchemy import pool
 from sqlalchemy.engine import Connection
 from sqlalchemy.ext.asyncio import create_async_engine
-import asyncio
 
-from app.core.database import Base
 from app.core.config import settings
-from shared.models import asset, asset_price, portfolio, refresh_sessions, trade, user
+from app.core.database import Base
+from shared.models import asset, asset_price, portfolio, refresh_sessions, trade, user  # noqa: F401
 
 
 config = context.config
@@ -15,14 +16,18 @@ config = context.config
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
 
-config.set_main_option("sqlalchemy.url", settings.DATABASE_URL)
-
 target_metadata = Base.metadata
 
 
+def get_url() -> str:
+    url = config.get_main_option("sqlalchemy.url")
+    return url or settings.DATABASE_URL
+
+
 def run_migrations_offline() -> None:
+    url = get_url()
     context.configure(
-        url=settings.DATABASE_URL,
+        url=url,
         target_metadata=target_metadata,
         literal_binds=True,
         dialect_opts={"paramstyle": "named"},
@@ -40,7 +45,7 @@ def do_run_migrations(connection: Connection) -> None:
 
 
 async def run_migrations_online() -> None:
-    connectable = create_async_engine(settings.DATABASE_URL, poolclass=pool.NullPool)
+    connectable = create_async_engine(get_url(), poolclass=pool.NullPool)
 
     async with connectable.connect() as connection:
         await connection.run_sync(do_run_migrations)
@@ -51,4 +56,12 @@ async def run_migrations_online() -> None:
 if context.is_offline_mode():
     run_migrations_offline()
 else:
-    asyncio.run(run_migrations_online())
+    try:
+        loop = asyncio.get_running_loop()
+    except RuntimeError:
+        loop = None
+
+    if loop and loop.is_running():
+        loop.create_task(run_migrations_online())
+    else:
+        asyncio.run(run_migrations_online())
